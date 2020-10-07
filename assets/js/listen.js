5ee3168e9cabff3a1b5bf908a8877a21d9b6b251
Sean McPherson <smcpherson@niche.com>
2020-10-07 15:04:17 -0400

    feat: listen mvp

--- assets/js/listen.js
+++ assets/js/listen.js
@@ -1,56 +1,60 @@
 import { WordsStore } from "./store.js";
-import { shuffle } from "./utils.js";
+import { el, shuffle } from "./utils.js";
 
 (() => {
-  // const again = document.getElementById("again");
-  // const end = document.getElementById("end");
-  // const fields = document.getElementById("fields");
-  // const wordForm = document.getElementById("word-form");
+  const again = document.getElementById("again");
+  const end = document.getElementById("end");
+  const fields = document.getElementById("fields");
+  const wordForm = document.getElementById("word-form");
   let words = shuffle(WordsStore.get());
-  let index = 0
-  const utterance = new SpeechSynthesisUtterance(words[index]); 
-  window.speechSynthesis.speak(utterance);
-  /**
-   * [Deprecation] speechSynthesis.speak() without user activation is no longer allowed since M71, around December 2018. See https://www.chromestatus.com/feature/5687444770914304 for more details
-   */
 
-  // function renderWords() {
-  //   fields.innerHTML = "";
-  //   words.forEach((word) => {
-  //     let scrambledWord = word;
-  //     while (scrambledWord === word) {
-  //       scrambledWord = shuffle(word.split("")).join("");
-  //     }
-  //     const field = document.createElement('div')
-  //     field.classList.add('field')
+  function renderWords() {
+    fields.innerHTML = "";
+    words.forEach((word, i) => {
+      const number = i + 1
+      let utterance = new SpeechSynthesisUtterance(word);
 
-  //     const label = document.createElement('label')
-  //     label.innerHTML = `Scrambled <b>${scrambledWord}</b>`;
-  //     label.htmlFor = word
-  //     field.appendChild(label)
+      const field = el("div", { class: "field" });
 
-  //     const input = document.createElement('input')
-  //     input.pattern = word
-  //     input.id = word
-  //     field.appendChild(input)
+      const button = el("button", { textContent: "Listen" });
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        speechSynthesis.speak(utterance);
+      });
 
-  //     fields.appendChild(field)
-  //   });
-  // }
+      field.appendChild(button);
 
-  // wordForm.addEventListener("submit", (event) => {
-  //   event.preventDefault();
-  //   end.removeAttribute("hidden");
-  //   wordForm.tabIndex = -1;
-  // });
+      const label = el("label", {
+        htmlFor: number,
+        innerHTML: `Word #${number}`,
+      });
+      field.appendChild(label);
 
-  // again.addEventListener("click", () => {
-  //   end.hidden = true;
-  //   words = shuffle(words);
-  //   renderWords();
-  //   wordForm.tabIndex = 0;
-  //   wordForm.focus();
-  // });
+      const input = el("input", {
+        autocomplete: 'chrome-off',
+        id: number,
+        pattern: word,
+        required: true,
+      });
+      field.appendChild(input);
 
-  // renderWords();
+      fields.appendChild(field);
+    });
+  }
+
+  wordForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    end.removeAttribute("hidden");
+    wordForm.tabIndex = -1;
+  });
+
+  again.addEventListener("click", () => {
+    end.hidden = true;
+    words = shuffle(words);
+    renderWords();
+    wordForm.tabIndex = 0;
+    wordForm.focus();
+  });
+
+  renderWords();
 })();
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,30 @@
+/**
+ * Creates a new element with given attributes and properties
+ * @param   {String} tag        The type of element to create
+ * @param   {Object} attributes An object of attributes to be applied
+ * @returns {Node}              A new node with applied attributes
+ */
+export function el(tag, attributes = {}) {
+  if (typeof document === undefined) return;
+
+  let element = document.createElement(tag);
+
+  for (let key in attributes) {
+    const value = attributes[key]
+    if (element[key] != null) {
+      element[key] = value
+    } else {
+      element.setAttribute(key, value);
+    }
+  }
+
+  return element;
+}
+
+/**
+ * Returns a shuffled shallow copy of an array
+ * @param {Array} array An array to shuffle
+ */
 export function shuffle(array) {
   return [...array].sort(() => Math.random() - 0.5);
 }
